dc7656f3d0162325dbb5cf41459e7e3afa9e3df0
kani <fongsilung@gmx.net>
2012-10-01 15:20:59 +0200

    Webcam working now

--- Form1.cs
+++ Form1.cs
@@ -20,7 +20,6 @@ namespace Pointboard
         //Constants
         const int N_CHESSFIELDS_X = 8;
         const int N_CHESSFIELDS_Y = 6;
-        const int SIZE_CHESSFIELD = 100;
         const string FILE_CHESSBOARD = @"..\..\files\Chessboard.png";
         const string FILE_TEST = @"..\..\files\Chessboard 2 Capture.jpg";
         const string FILE_TEST2 = @"..\..\files\Test_Image_2.png";
@@ -71,34 +70,43 @@ namespace Pointboard
 
         private void Show_cam(object sender, EventArgs e)
         {
+            //Load image and display webcam in box_original
+            //image_original = new Image<Bgr, byte>(FILE_TEST);
+            image_original = webcam.QueryFrame();
+            box_original.Image = image_original.ToBitmap();
+
             if (!calibrated)
             {
-                //Load image and display webcam in box_original
-                image_original = new Image<Bgr, byte>(FILE_TEST);
-                //image_original = webcam.QueryFrame();
-                box_original.Image = image_original.ToBitmap();
-
                 btn_calibrate.Enabled = true;
             }
             else
             {
+                //Transform and resize image
+                image_transformed = image_original.WarpPerspective(t_matrix, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC, Emgu.CV.CvEnum.WARP.CV_WARP_FILL_OUTLIERS, new Bgr(Color.Green));
+
+                //lbl_info.Text = image_original.Size.Width + "-" + image_original.Size.Height + " " + image_transformed.Size.Width + "-" + image_transformed.Size.Height;
+
+                //Display in box_transformed
+                box_transformed.Image = image_transformed.ToBitmap();
+
                 //Get red
-                image_filtered = image_transformed.SmoothBlur(5, 5).InRange(new Bgr(Color.DarkRed), new Bgr(Color.FromArgb(255, 100, 100)));
+                image_filtered = image_transformed.SmoothBlur(5, 5).InRange(new Bgr(Color.DarkRed), new Bgr(Color.White));//Color.FromArgb(255, 100, 100)));
                 box_filtered.Image = image_filtered.ToBitmap();
 
-                //Find_circles();
+                Find_circles();
             }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //image_transformed = image_original;
             calibrated = Calibrate_perspective();
         }
 
         private bool Calibrate_perspective()
         {
-            //Load and display chessboard image for calibration
-            Image<Gray, Byte> image_chessboard = new Image<Gray, byte>(FILE_CHESSBOARD);
+            //Load (with same size as original) and display chessboard image for calibration
+            Image<Gray, Byte> image_chessboard = new Image<Gray, byte>(FILE_CHESSBOARD).Resize(image_original.Width, image_original.Height, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
             box_final.Image = image_chessboard.ToBitmap();
 
             //Get corner-points of original and captured chessboard
@@ -116,16 +124,6 @@ namespace Pointboard
             //Create matrix for transformation
             t_matrix = CameraCalibration.FindHomography(corners_src, corners_dst, Emgu.CV.CvEnum.HOMOGRAPHY_METHOD.DEFAULT, 1);
 
-            //Transform and resize image
-            image_transformed = image_original.WarpPerspective(t_matrix, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC, Emgu.CV.CvEnum.WARP.CV_WARP_FILL_OUTLIERS, new Bgr(Color.Green));
-            Rectangle sub_image = new Rectangle(0, 0, Convert.ToInt32(corners_dst[corners_dst.Length - 1].X + SIZE_CHESSFIELD), Convert.ToInt32(corners_dst[corners_dst.Length - 1].Y + SIZE_CHESSFIELD));
-            image_transformed = image_transformed.GetSubRect(sub_image).Resize(image_original.Size.Width, image_original.Size.Height, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
-
-            //lbl_info.Text = image_original.Size.Width + "-" + image_original.Size.Height + " " + image_transformed.Size.Width + "-" + image_transformed.Size.Height;
-
-            //Display in box_transformed
-            box_transformed.Image = image_transformed.ToBitmap();
-
             //Clear box_final
             box_final.Image = null;
             box_final.BackColor = Color.Black;
@@ -137,10 +135,11 @@ namespace Pointboard
         {
             //Clear image
             //drawings.DrawImage(box_final.Image, 0, 0); //maybe backgroundimage
+            drawings.Clear(box_final.BackColor);
 
             //Find Circles
             CircleF[] circles = image_filtered.HoughCircles(
-            new Gray(180), //The higher threshold of the two passed to Canny edge detector (the lower one will be twice smaller)
+            new Gray(180), //The higher threshold of the two passed to Canny edge detector (dthe lower one will be twice smaller)
             new Gray(6), //Accumulator threshold at the center detection stage
             1.0, //Resolution of the accumulator used to detect centers of the circles
             10.0, //Min distance
@@ -151,7 +150,7 @@ namespace Pointboard
             //Mark first circle
             if (circles.Length > 0)
             {
-                Pen pen_circle = new Pen(Color.Red, 3);
+                Pen pen_circle = new Pen(Color.Blue, 3);
                 drawings.DrawEllipse(pen_circle, circles[0].Center.X - circles[0].Radius, circles[0].Center.Y - circles[0].Radius, circles[0].Radius * 2, circles[0].Radius * 2);
             }
 
